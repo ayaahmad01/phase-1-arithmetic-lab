4cc1d54abe68b62be1eb0c5ccb8cf7c9dbec2230
Aya Abu AlHummos <ayaahmad113@gmail.com>
2023-10-19 22:26:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2 
+console.log(multiply) 
+const random = Math.floor(Math.random() > 0)
+console.log(random) 
+let num3 = 7
+let num4 = 3
+const mod = num3 - num4
+console.log(mod)
+const max = Math.max(0, 18, 6, 11, 20)
+console.log(max)
 // Write your code here
